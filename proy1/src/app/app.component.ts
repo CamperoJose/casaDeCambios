c4c23abb0c9eb270aff654dfe9eed8f7606c3743
CamperoJose <113204362+jac2112@users.noreply.github.com>
2022-10-20 11:54:21 -0400

    cambios

--- proy1/src/app/app.component.ts
+++ proy1/src/app/app.component.ts
@@ -7,24 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proy1';
-  bs ;
-  dolar;
-  euro;
-  soles;
+  bs:any;
+  dolar:any;
+  euro:any;
+  soles:any;
 
-  getValor(val) {
+
+  getValor(val: any) {
     console.warn(val);
     this.bs = parseInt(val);
   }
-  toDolar(valor) {
+  toDolar(valor: any) {
     this.dolar = (parseFloat(this.bs) / 6.96).toFixed(2);
     console.warn(this.dolar);
   }
-  toEuro(valor) {
+  toEuro(valor: any) {
     this.euro = (parseFloat(this.bs) / 6.82).toFixed(2);
     return this.euro;
   }
-  toSol(valor) {
+  toSol(valor: any) {
    
 
     this.soles = (parseFloat(this.bs) / 1.74).toFixed(2);
