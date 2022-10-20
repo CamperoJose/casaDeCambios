16744c6a5196f6860b713b500a4b84afd8fbb580
Sergio Guachalla <sergioguachalla0@gmail.com>
2022-10-20 11:42:06 -0400

    Adición de las funciones para calcular el cambio

--- proy1/src/app/app.component.ts
+++ proy1/src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proy1';
+  bs ;
+  dolar;
+  euro;
+  soles;
+
+  getValor(val) {
+    console.warn(val);
+    this.bs = parseInt(val);
+  }
+  toDolar(valor) {
+    this.dolar = (parseFloat(this.bs) / 6.96).toFixed(2);
+    console.warn(this.dolar);
+  }
+  toEuro(valor) {
+    this.euro = (parseFloat(this.bs) / 6.82).toFixed(2);
+    return this.euro;
+  }
+  toSol(valor) {
+   
+
+    this.soles = (parseFloat(this.bs) / 1.74).toFixed(2);
+    return this.soles;
+  }
 }
